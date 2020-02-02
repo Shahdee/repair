f23cd3b7cfed183fa4fdd67fe6e360ef272862dc
i.nikulin <inikdesign@gmail.com>
2020-02-02 13:17:32 +0300

    take and put sounds

---
+++ repair/Assets/Scripts/SoundRandomContainer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class SoundRandomContainer : MonoBehaviour
+{
+
+    [SerializeField] private AudioClip[] audioClips;
+    
+
+    private AudioSource source;
+    public void Play()
+    {
+        if(audioClips!=null)
+        {
+            int value = Random.Range(0, audioClips.Length - 1);
+            source.clip = audioClips[value];
+            source.Play();
+        }
+        
+    }
+
+    public void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+}
